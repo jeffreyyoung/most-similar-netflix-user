ddc592bf5f288f816e2f5bd5aee6677352068cfd
Jeffrey Young <jeffreeyoung@yahoo.com>
2015-10-12 17:44:21 -0600

    updating

--- NetflixUserMatcherMapper.java
+++ NetflixUserMatcherMapper.java
@@ -59,9 +59,10 @@ public class NetflixUserMatcherMapper extends MapReduceBase
     {
       return (!entryUserID.trim().equals(userID.trim())
               && userData.containsKey(showID.trim())
-              && (Integer.parseInt(userData.get(showID.trim())) == stars
+              && (
+                        Integer.parseInt(userData.get(showID.trim())) == stars
                     || (Integer.parseInt(userData.get(showID.trim())) == (stars - 1))
-                    || (Integer.parseInt(userData.get(showID.trim())) == (stars + 1));
+                    || (Integer.parseInt(userData.get(showID.trim())) == (stars + 1)));
     }
 
     @Override
